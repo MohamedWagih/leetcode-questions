c779aa791b7b84f1edab700afbbf9e50f2330e94
Mohamed Wagih <mohamed0wagih@gmail.com>
2022-02-14 02:48:30 +0200

    Time: 30 ms (83.77%), Space: 13.9 MB (74.25%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0 or n ==1:
+            return n
+        
+        memo = [0] * (n+1)
+        memo[0] = 0
+        memo[1] = 1
+        
+        for i in range(2, n+1):
+            memo[i] = memo[i-1] + memo[i-2]
+        
+        return memo[n]
