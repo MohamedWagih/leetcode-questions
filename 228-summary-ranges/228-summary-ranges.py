9c6eed3c54ac65479bdaecc9e7bd162abd12aaa9
Mohamed Wagih <mohamed0wagih@gmail.com>
2022-03-13 11:42:50 +0200

    Time: 24 ms (98.55%), Space: 14 MB (7.51%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.py
+++ 228-summary-ranges/228-summary-ranges.py
@@ -4,21 +4,17 @@ class Solution:
         if len(nums) == 0:
             return result
         
-        currRange = []
-        for i in range(len(nums)):
-            if i==0 or (i > 0 and nums[i]-1 == nums[i-1]):
-                currRange.append(nums[i])
-                continue
-            else:
-                if currRange[0] == currRange[-1]:
-                    result.append(str(currRange[0]))
-                else:
-                    result.append(str(currRange[0]) + "->" + str(currRange[-1]))
-                currRange = [nums[i]]
-                
-        if currRange[0] == currRange[-1]:
-            result.append(str(currRange[0]))
-        else:
-            result.append(str(currRange[0]) + "->" + str(currRange[-1]))
+        start, end = 0,0
+        while end < len(nums):
+            while end+1 < len(nums) and nums[end]+1 == nums[end+1]:
+                end += 1
             
+            if start == end:
+                result.append(str(nums[start]))
+            else:
+                result.append(str(nums[start]) + "->" + str(nums[end]))
+            
+            end += 1
+            start = end
+                
         return result
