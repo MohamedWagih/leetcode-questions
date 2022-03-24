f8855321f8fb6fce10f5e765b32a0103fdefe849
Mohamed Wagih <mohamed0wagih@gmail.com>
2022-03-24 12:13:24 +0200

    Time: 928 ms (72.65%), Space: 17.9 MB (79.57%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,21 +1,20 @@
 class Solution:
-    def pair_with_target_Sum(self, target_sum, nums, left, triplets):
-        right = len(nums) - 1
-        while left < right:
+    def pair_with_target_Sum(self, target, nums, left, triplets):
+        right = len(nums)-1
+        while left<right:
             curr_sum = nums[left] + nums[right]
-            
-            if curr_sum == target_sum :
-                triplets.append([-target_sum, nums[left], nums[right]])
+            if curr_sum == target:
+                triplets.append([-target, nums[left], nums[right]])
                 left += 1
                 right -= 1
-                while left < right and nums[left] == nums[left-1]:
+                while left<right and nums[left] == nums[left-1]:
                     left += 1
-                while left < right and nums[right] == nums[right+1]:
+                while left<right and nums[right] == nums[right+1]:
                     right -= 1
-            elif curr_sum < target_sum:
-                left += 1
+            elif curr_sum > target:
+                right -= 1
             else:
-                right -= 1 
+                left += 1
                 
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
@@ -23,7 +22,7 @@ class Solution:
         for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-            self.pair_with_target_Sum(-nums[i], nums, i+1, triplets )
+            self.pair_with_target_Sum(-nums[i], nums, i+1, triplets)
         return triplets 
     
     
