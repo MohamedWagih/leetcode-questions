4c4024f24e10222b0f25b1215d3e988128056460
Mohamed Wagih <mohamed0wagih@gmail.com>
2022-04-21 05:04:17 +0200

    Time: 141 ms (86.30%), Space: 14.1 MB (95.63%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -1,16 +1,42 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        rows, cols= len(grid), len(grid[0])
+        m = len(grid)
+        n = len(grid[0])
+
+        count = 0
         visited = set()
-        
-        def dfs(row, col):
-            if (row < 0 or row == rows or col < 0 or col == cols or grid[row][col] == 0 or (row, col) in visited):
+    
+        def visit(row, col, queue):
+            if min(row,col) < 0 or row==m or col==n:
                 return 0
-            visited.add((row, col))
-            return (1 + dfs(row + 1, col) + dfs(row - 1, col)+ dfs(row, col + 1)+ dfs(row, col - 1))
+            
+            if grid[row][col] == 1:
+                queue.append((row, col))
+                grid[row][col] = 2
+                return 1
+            else:
+                return 0
         
-        maxArea = 0
-        for row in range(rows):
-            for col in range(cols):
-                maxArea  = max(maxArea, dfs(row, col))
-        return maxArea
+        def bfs(row, col):
+            queue = deque()
+            queue.append((row,col))
+            grid[row][col] = 2
+            area = 1
+            while queue:
+                x = len(queue)
+                for _ in range(x):
+                    curr_row, curr_col = queue.popleft()
+                    area += visit(curr_row+1, curr_col, queue)
+                    area += visit(curr_row-1, curr_col, queue)
+                    area += visit(curr_row, curr_col+1, queue)
+                    area += visit(curr_row, curr_col-1, queue)
+            return area
+
+        max_area = 0
+        for row in range(m):
+            for col in range(n):
+                if grid[row][col] == 1:
+                    area = bfs(row,col)
+                    max_area = max(area, max_area)
+                    
+        return max_area 
