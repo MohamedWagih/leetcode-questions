5ad417c72a107372c57c27933cc55b2e0e03f81d
Mohamed Wagih <mohamed0wagih@gmail.com>
2022-05-11 02:29:11 +0200

    Time: 39 ms (97.72%), Space: 14.4 MB (45.34%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,26 +1,39 @@
-def get_value(pos, rows, cols, mat):
-    row = pos//cols
-    col = pos%cols
-    return mat[row][col]
 
 class Solution:
-    
+    # time: O( log m + log m) == O( log m*n )
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         rows, cols = len(matrix), len(matrix[0])
         
-        low = 0
-        high = (rows * cols) - 1
-        
-        while low <= high:
-            mid = (high + low) // 2 
-            mid_val = get_value(mid, rows, cols, matrix)
+        # 1) serch for the right row
+        # time: O(log m)
+        top, bottom = 0, rows-1
+        while top <= bottom:
+            mid_row = top + (bottom-top)//2
             
-            if mid_val == target:
+            if target < matrix[mid_row][0]:
+                bottom = mid_row-1
+            elif target > matrix[mid_row][-1]:
+                top = mid_row+1
+            else:
+                break
+                
+        # if did not find a suitable row return False
+        if not (top <= bottom):
+            return False
+        
+        # 2) search for target in the suitable row we found
+        # time: O(log n)
+        mid_row = top + (bottom-top)//2
+        left, right = 0, cols-1
+        while left <= right:
+            mid = left + (right-left)//2
+            
+            if target == matrix[mid_row][mid]:
                 return True
             
-            elif mid_val < target:
-                low = mid + 1
-            else:
-                high = mid - 1
-                
+            if target < matrix[mid_row][mid]:
+                right = mid - 1
+            elif target > matrix[mid_row][mid]:
+                left = mid + 1
+            
         return False
