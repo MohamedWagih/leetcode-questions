0b60cbea02f23e527d1105263b73738e7b936489
Mohamed Wagih <mohamed0wagih@gmail.com>
2022-05-26 11:02:45 +0200

    Time: 1340 ms (78.02%), Space: 61.7 MB (46.50%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.py
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.py
@@ -4,14 +4,18 @@ class Solution:
         if n == 0:
             return 0
         
+        # we sorted it with (width) so we are left with the (height) to find the longest increasing (height)
+        # but we sort in deacreasing (height) in case of equal (width) 
         envelopes.sort(key= lambda env: (env[0], -env[1]))
         dp = []
         
         for w, h in envelopes:
-            left = bisect_left(dp, h)
-            if left == len(dp):
-                dp.append(h)
+            # find the position to insert the new H 
+            i = bisect_left(dp, h)
+            
+            if i == len(dp):
+                dp.append(h)    
             else:
-                dp[left] = h
+                dp[i] = h
                 
         return len(dp)
