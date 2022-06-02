be867c1b385717f1ac79554e4f6067831f516857
Mohamed Wagih <mohamed0wagih@gmail.com>
2022-06-02 11:15:39 +0200

    Time: 60 ms (22.06%), Space: 14.2 MB (33.41%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,10 +1,17 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        all_subsets = [[]]
+        n = len(nums)
+        all_subsets = []
         
-        for num in nums:
-            subsets_length = len(all_subsets)
-            for i in range(subsets_length):
-                all_subsets.append(all_subsets[i] + [num])
+        def backtrack(idx, curr_subset):
+            all_subsets.append(curr_subset[:])
+            
+            for i in range(idx, n):
+                curr_subset.append(nums[i])
+                backtrack(i+1, curr_subset)
+                curr_subset.pop()
+        
+        
+        backtrack(0,[])
         
         return all_subsets
