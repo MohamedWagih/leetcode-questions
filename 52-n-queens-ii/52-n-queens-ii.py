577c622e6acb84f94369e41126e78c3d2af21940
Mohamed Wagih <mohamed0wagih@gmail.com>
2022-06-06 10:26:01 +0200

    Time: 78 ms (53.37%), Space: 14.2 MB (5.25%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.py
@@ -0,0 +1,40 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        
+        # no two queens can be in the same column so we keep track of visited columns
+        visited_cols = set()
+        
+        # no two queens can be in the same positive diagonal so we keep track of visited positive diagonals
+        # positive diagonal can be calculated as ( row - col )
+        visited_diagonal_pos = set()
+        
+        # no two queens can be in the same negative diagonal so we keep track of visited negative diagonals
+        # positive diagonal can be calculated as ( row + col )
+        visited_diagonal_neg = set()
+        
+        board = [['.'] * n for _ in range(n)]
+        solutions = []
+        
+        def backtrack(row):
+            if row == n:
+                solutions.append([''.join(r) for r in board])
+                return
+            
+            for col in range(n):
+                diag_pos = row + col
+                diag_neg = row - col
+                if not (col in visited_cols or diag_pos in visited_diagonal_pos or diag_neg in visited_diagonal_neg):
+                    visited_cols.add(col)
+                    visited_diagonal_pos.add(diag_pos)
+                    visited_diagonal_neg.add(diag_neg)
+                    board[row][col] = "Q"
+                    
+                    backtrack(row+1)
+                    
+                    visited_cols.remove(col)
+                    visited_diagonal_pos.remove(diag_pos)
+                    visited_diagonal_neg.remove(diag_neg)
+                    board[row][col] = "."
+                    
+        backtrack(0)
+        return len(solutions)
