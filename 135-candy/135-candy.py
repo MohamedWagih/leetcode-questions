6b10247565b8028f681a072af0ca756c344f357b
Mohamed Wagih <mohamed0wagih@gmail.com>
2022-07-04 12:05:43 +0200

    Time: 312 ms (23.94%), Space: 16.9 MB (37.74%) - LeetHub

--- 135-candy/135-candy.py
+++ 135-candy/135-candy.py
@@ -1,21 +1,22 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         n = len(ratings)
-        ltr = [1 for _ in range(n)]
-        rtl = [1 for _ in range(n)]
+        candies = [1 for _ in range(n)]
         
         # left to right traversal
         for i in range(1, n):
             if ratings[i] > ratings[i-1]:
-                ltr[i] = ltr[i-1] + 1
+                candies[i] = candies[i-1] + 1
+        
+        # last element already have no elements on its right 
+        # so we include it in the sum
+        total_candies = candies[-1]
         
         # right to left traversal
         for i in range(n-2, -1, -1):
             if ratings[i] > ratings[i+1]:
-                rtl[i] = rtl[i+1] + 1
-        
-        total_candies = 0
-        for i in range(n):
-            total_candies += max(ltr[i], rtl[i])
+                candies[i] = max(candies[i], candies[i+1] + 1)
+            
+            total_candies += candies[i]
         
         return total_candies
