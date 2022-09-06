69151aa69ac54654106ef3afdd5d0059ee0fd5dc
Mohamed Wagih <mohamed0wagih@gmail.com>
2022-09-07 01:43:40 +0200

    Time: 57 ms (32.01%), Space: 13.9 MB (69.11%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.py
+++ 814-binary-tree-pruning/814-binary-tree-pruning.py
@@ -7,28 +7,20 @@
 class Solution:
     def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         def dfs(root):
-            left_has_ones = False
-            right_has_ones = False
+            if not root:
+                return False
             
-            if not root.left and not root.right:
-                return root.val == 1
+            left_has_ones = dfs(root.left)
+            right_has_ones = dfs(root.right)
             
-            if root.left:
-                left_has_ones = dfs(root.left)
-                if not left_has_ones:
-                    root.left = None
-                    
-            if root.right:
-                right_has_ones = dfs(root.right)
-                if not right_has_ones:
-                    root.right = None
+            if not left_has_ones:
+                root.left = None
+
+            if not right_has_ones:
+                root.right = None
             
             return left_has_ones or right_has_ones or root.val == 1
         
-        has_ones = dfs(root)
-        if not has_ones:
-            root = None
-        
-        return root
+        return root if dfs(root) else None
                 
             
