f5ee50e32c04c0656774efe220b44800dc032357
Mohamed Wagih <mohamed0wagih@gmail.com>
2022-10-24 10:32:19 +0200

    Time: 50 ms (91.36%), Space: 14 MB (82.00%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -3,17 +3,15 @@ class Solution:
         left = 0
         right = len(nums)-1
         
-        while left < right:
-            mid = left + (right - left) // 2
+        while left < right: 
+            mid = left + ( right - left ) // 2
             
-            if nums[mid-1] < nums[mid] > nums[mid+1]:
-                return mid
-            
-            elif nums[mid+1] > nums[mid]:
+            if nums[mid+1] > nums[mid]:
                 left = mid + 1
             
             else:
-                right = mid - 1 
+                right = mid
         
-        return left
+        return left 
+            
         
